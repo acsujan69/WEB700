b9a320fbd7b2dc471e693714c33632ee1db0f471
Sujan Acharya <acsujan69@gmail.com>
2024-05-28 12:52:50 -0400

    Assignment 2 Final

--- Assignment 2/a2.js
+++ Assignment 2/a2.js
@@ -4,7 +4,7 @@
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
 * 
-*  Name: Sujan Acharya              Student ID: 152904231                 Date: 21 May, 2024
+*  Name: Sujan Acharya              Student ID: 152904231                 Date: 28 May, 2024
 *
 ********************************************************************************/ 
 
